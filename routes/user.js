e33a0516008618c193abefe83d35728e4a2d214c
Viet Le <viethle126@gmail.com>
2016-05-09 00:36:41 -0700

    Added /user/logout #1

--- routes/user.js
+++ routes/user.js
@@ -12,37 +12,86 @@ router.post('/', function(req, res) {
       res.json({ info: 'Error during generate hash', error: error });
       return;
     }
-    var data = {
-      user: req.body.user,
-      hash: hash,
-    }
-    var user = new User(data);
+
+    var user = new User({ user: req.body.user, hash: hash });
     user.save(function(error) {
       if (error) {
         res.json({ info: 'Error during user sign up', error: error });
         return;
       }
+
+      res.cookie('user', req.body.user);
+      res.cookie('session', makeCookie());
       res.json({ info: 'User signed up successfully' });
     })
   })
 })
 
 router.post('/login', function(req, res) {
+  var session = makeCookie();
+
   User.find({ user: req.body.user }, function(error, results) {
     if (error) {
       res.json({ info: 'Error during find user', error: error });
       return;
     }
+
+    var user = results[0];
     var password = req.body.password;
-    var hash = results[0].hash
+    var hash = user.hash;
+
     bcrypt.compare(password, hash, function(error, results) {
       if (results === true) {
-        res.json({ info: 'User logged in successfully' });
+        user.session = session;
+        user.save(function(error) {
+          if (error) {
+            throw new Error(error);
+          }
+        })
+
+        res.cookie('user', req.body.user);
+        res.cookie('session', session);
+        res.json({ info: 'User logged in successfully', session });
         return;
       }
+
       res.json({ info: 'User log in failed; Wrong password' });
-    });
+    })
   })
-});
+})
+
+router.post('/logout', function(req, res) {
+  User.find({ user: req.body.user }, function(error, results) {
+    if (error) {
+      res.json({ info: 'Error during find user', error: error });
+      return;
+    }
+
+    user = results[0];
+    user.session = null;
+    user.save(function(error) {
+      if (error) {
+        throw new Error(error);
+      }
+    })
+
+    res.clearCookie('user');
+    res.clearCookie('session');
+    res.json({ info: 'User logged in successfully' });
+  })
+})
+
+// generate session id cookie
+function makeCookie() {
+  var chars = '0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz';
+  var cookie = '';
+
+  while (cookie.length < 32) {
+    var pick = Math.floor(Math.random() * 62);
+    cookie += chars[pick];
+  }
+  
+  return cookie;
+}
 
 module.exports = router;
