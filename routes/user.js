32af5571e45e0cd736ceb553e34ec5c7dcf1cecc
Viet Le <viethle126@gmail.com>
2016-05-08 23:30:35 -0700

    Added /user/login #1

--- routes/user.js
+++ routes/user.js
@@ -22,9 +22,27 @@ router.post('/', function(req, res) {
         res.json({ info: 'Error during user sign up', error: error });
         return;
       }
-      res.json({ info: 'User signed up successfully'});
+      res.json({ info: 'User signed up successfully' });
     })
   })
 })
+
+router.post('/login', function(req, res) {
+  User.find({ user: req.body.user }, function(error, results) {
+    if (error) {
+      res.json({ info: 'Error during find user', error: error });
+      return;
+    }
+    var password = req.body.password;
+    var hash = results[0].hash
+    bcrypt.compare(password, hash, function(error, results) {
+      if (results === true) {
+        res.json({ info: 'User logged in successfully' });
+        return;
+      }
+      res.json({ info: 'User log in failed; Wrong password' });
+    });
+  })
+});
 
 module.exports = router;
