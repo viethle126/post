e5b3a4f5c8d2374dad2360c70bd8aea44e041615
Viet Le <viethle126@gmail.com>
2016-05-09 18:06:27 -0700

    Added Route Provider #2

--- public/app.js
+++ public/app.js
@@ -1,1 +1,17 @@
-var app = angular.module('post', []);
+var app = angular.module('post', ['ngRoute']);
+
+app.config(['$routeProvider', function($routeProvider) {
+  $routeProvider
+  .when('/submit', {
+    templateUrl: 'views/submit.view.html',
+  })
+  .when('/home', {
+    templateUrl: 'views/home.view.html',
+  })
+  .when('/saved', {
+    templateUrl: 'views/saved.view.html',
+  })
+  .otherwise({
+    redirectTo: '/home'
+  })
+}]);
