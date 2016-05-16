175eb82e40be5c8d7a7d1d698546faf3f5b174d9
Viet Le <viethle126@gmail.com>
2016-05-15 23:32:59 -0700

    Added Routes for Upvoting/Downvoting Comments #5

--- routes/vote.js
+++ routes/vote.js
@@ -1,8 +1,9 @@
 var router = require('express').Router();
 // mongoose
+var Comment = require('../models/comment');
 var Post = require('../models/post');
 
-// add vote
+// add vote to post
 router.put('/post', function(req, res) {
   Post.findOne({ _id: req.body.post_id }, function(error, post) {
     if (error) {
@@ -29,7 +30,34 @@ router.put('/post', function(req, res) {
     }
   })
 })
-// clear votes
+// add vote to comment
+router.put('/comment', function(req, res) {
+  Comment.findOne({ _id: req.body.post_id }, function(error, comment) {
+    if (error) {
+      res.json({ info: 'Error during find comment' });
+      return;
+    }
+
+    if (comment === null) {
+      res.json({ info: 'Could not find comment' });
+      return;
+    }
+
+    if (comment[req.body.type].indexOf(req.currentUser.toString()) === -1) {
+      comment[req.body.type].push(req.currentUser.toString());
+      comment.save(function(error) {
+        if (error) {
+          res.json({ info: 'Error during update comment', error: error });
+          return;
+        }
+        res.status(200).json({ info: 'Vote added successfully' });
+      })
+    } else {
+      res.status(200).json({ info: 'Duplicate vote was not added' });
+    }
+  })
+})
+// clear votes, post
 router.delete('/post', function(req, res) {
   Post.findOne({ _id: req.body.post_id }, function(error, post) {
     if (error) {
@@ -53,6 +81,36 @@ router.delete('/post', function(req, res) {
     post.save(function(error) {
       if (error) {
         res.json({ info: 'Error during update post', error: error });
+        return;
+      }
+      res.status(200).json({ info: 'Vote cleared successfully' });
+    })
+  })
+})
+// clear votes, comment
+router.delete('/comment', function(req, res) {
+  Comment.findOne({ _id: req.body.post_id }, function(error, comment) {
+    if (error) {
+      res.json({ info: 'Error during find comment' });
+      return;
+    }
+    if (comment === null) {
+      res.json({ info: 'Could not find comment' });
+      return;
+    }
+    var up = comment.upvotes.indexOf(req.currentUser.toString());
+    var down = comment.downvotes.indexOf(req.currentUser.toString());
+    if (up !== -1) {
+      comment.upvotes.splice(up, 1);
+    }
+
+    if (down !== -1) {
+      comment.downvotes.splice(down, 1);
+    }
+
+    comment.save(function(error) {
+      if (error) {
+        res.json({ info: 'Error during update comment', error: error });
         return;
       }
       res.status(200).json({ info: 'Vote cleared successfully' });
