68ffd01daa9aee2b9f38f6d737e50b3a45aec9a5
Viet Le <viethle126@gmail.com>
2016-05-26 01:44:34 -0700

    Refactor routes/vote.js #26

--- routes/vote.js
+++ routes/vote.js
@@ -4,6 +4,119 @@ var User = require('../models/user');
 var Post = require('../models/post');
 var Comment = require('../models/comment');
 
+function handleVote(doc, type, user) {
+  if (doc[type].indexOf(user) !== -1) {
+    res.status(200).json({ info: 'Duplicate vote was not added' });
+    return;
+  } else {
+    doc[type].push(user);
+
+    if (type === 'upvotes') {
+      doc.score++;
+    }
+
+    if (type === 'downvotes') {
+      doc.score--;
+    }
+
+    return doc;
+  }
+}
+
+function updateUser(user_id, type, res) {
+  User.findOne({ _id: user_id }, function(error, user) {
+    if (error) {
+      res.json({ info: 'Error during find user', error: error });
+    }
+
+    if (user === null) {
+      res.status(200).json({ info: 'Vote added successfully' });
+      return;
+    }
+
+    if (type === 'upvotes') {
+      user.upvotes++;
+      user.score++;
+    }
+
+    if (type === 'downvotes') {
+      user.downvotes++;
+      user.score--;
+    }
+
+    user.save(function(error) {
+      if (error) {
+        res.json({ info: 'Error during update user upvote count', error: error });
+      }
+
+      res.status(200).json({ info: 'Vote added successfully' });
+    })
+  })
+}
+
+function configure(doc, user) {
+  var upvote = doc.upvotes.indexOf(user);
+  var downvote = doc.downvotes.indexOf(user);
+  var config = {};
+
+  if (upvote !== -1) {
+    config.type = 'upvotes';
+    config.index = upvote;
+  }
+
+  if (downvote !== -1) {
+    config.type = 'downvotes';
+    config.index = downvote;
+  }
+
+  return config;
+}
+
+function clearVotes(doc, user, config) {
+  doc[config.type].splice(config.index, 1);
+
+  if (config.type === 'upvotes') {
+    doc.score--;
+    return doc;
+  }
+
+  if (config.type === 'downvotes') {
+    doc.score++;
+    return doc;
+  }
+}
+
+function clearUser(user_id, config, res) {
+  User.findOne({ _id: user_id }, function(error, user) {
+    if (error) {
+      res.json({ info: 'Error during find user', error: error });
+    }
+
+    if (user === null) {
+      res.status(200).json({ info: 'Vote cleared successfully' });
+      return;
+    }
+
+    if (config.type === 'upvotes') {
+      user.upvotes--;
+      user.score--;
+    }
+
+    if (config.type === 'downvotes') {
+      user.downvotes--;
+      user.score++;
+    }
+
+    user.save(function(error) {
+      if (error) {
+        res.json({ info: 'Error during update user upvote count', error: error });
+      }
+
+      res.status(200).json({ info: 'Vote cleared successfully' });
+    })
+  })
+}
+
 // add vote to post
 router.put('/post', function(req, res) {
   Post.findOne({ _id: req.body.post_id }, function(error, post) {
@@ -17,50 +130,16 @@ router.put('/post', function(req, res) {
       return;
     }
 
-    if (post[req.body.type].indexOf(req.currentUser.toString()) === -1) {
-      post[req.body.type].push(req.currentUser.toString());
+    post = handleVote(post, req.body.type, req.currentUser.toString(), res);
 
-      if (req.body.type === 'upvotes') {
-        post.score++;
+    post.save(function(error) {
+      if (error) {
+        res.json({ info: 'Error during update post', error: error });
+        return;
       }
 
-      if (req.body.type === 'downvotes') {
-        post.score--;
-      }
-
-      post.save(function(error) {
-        if (error) {
-          res.json({ info: 'Error during update post', error: error });
-          return;
-        }
-
-        User.findOne({ _id: post.user_id }, function(error, user) {
-          if (error) {
-            res.json({ info: 'Error during find user', error: error });
-          }
-
-          if (req.body.type === 'upvotes') {
-            user.upvotes++;
-            user.score++;
-          }
-
-          if (req.body.type === 'downvotes') {
-            user.downvotes++;
-            user.score--;
-          }
-
-          user.save(function(error) {
-            if (error) {
-              res.json({ info: 'Error during update user upvote count', error: error });
-            }
-
-            res.status(200).json({ info: 'Vote added successfully' });
-          })
-        })
-      })
-    } else {
-      res.status(200).json({ info: 'Duplicate vote was not added' });
-    }
+      updateUser(post.user_id, req.body.type, res);
+    })
   })
 })
 // add vote to comment
@@ -76,77 +155,35 @@ router.put('/comment', function(req, res) {
       return;
     }
 
-    if (comment[req.body.type].indexOf(req.currentUser.toString()) === -1) {
-      comment[req.body.type].push(req.currentUser.toString());
+    comment = handleVote(comment, req.body.type, req.currentUser.toString(), res);
 
-      if (req.body.type === 'upvotes') {
-        comment.score++;
+    comment.save(function(error) {
+      if (error) {
+        res.json({ info: 'Error during update comment', error: error });
+        return;
       }
 
-      if (req.body.type === 'downvotes') {
-        comment.score--;
-      }
-
-      comment.save(function(error) {
-        if (error) {
-          res.json({ info: 'Error during update comment', error: error });
-          return;
-        }
-
-        User.findOne({ _id: comment.user_id }, function(error, user) {
-          if (error) {
-            res.json({ info: 'Error during find user', error: error });
-          }
-
-          if (req.body.type === 'upvotes') {
-            user.upvotes++;
-            user.score++;
-          }
-
-          if (req.body.type === 'downvotes') {
-            user.downvotes++;
-            user.score--;
-          }
-
-          user.save(function(error) {
-            if (error) {
-              res.json({ info: 'Error during update user upvote count', error: error });
-            }
-
-            res.status(200).json({ info: 'Vote added successfully' });
-          })
-        })
-      })
-    } else {
-      res.status(200).json({ info: 'Duplicate vote was not added' });
-    }
+      updateUser(comment.user_id, req.body.type, res);
+    })
   })
 })
 // clear votes, post
 router.delete('/post', function(req, res) {
   Post.findOne({ _id: req.body.post_id }, function(error, post) {
-    var type;
-
     if (error) {
       res.json({ info: 'Error during find post' });
       return;
     }
+
     if (post === null) {
       res.json({ info: 'Could not find post' });
       return;
     }
-    var up = post.upvotes.indexOf(req.currentUser.toString());
-    var down = post.downvotes.indexOf(req.currentUser.toString());
-    if (up !== -1) {
-      post.upvotes.splice(up, 1);
-      post.score--;
-      type = 'upvotes';
-    }
 
-    if (down !== -1) {
-      post.downvotes.splice(down, 1);
-      post.score++;
-      type = 'downvotes';
+    var config = configure(post, req.currentUser.toString());
+
+    if (config.type) {
+      post = clearVotes(post, req.currentUser.toString(), config);
     }
 
     post.save(function(error) {
@@ -155,57 +192,27 @@ router.delete('/post', function(req, res) {
         return;
       }
 
-      User.findOne({ _id: post.user_id }, function(error, user) {
-        if (error) {
-          res.json({ info: 'Error during find user', error: error });
-        }
-
-        if (type === 'upvotes') {
-          user.upvotes--;
-          user.score--;
-        }
-
-        if (type === 'downvotes') {
-          user.downvotes--;
-          user.score++;
-        }
-
-        user.save(function(error) {
-          if (error) {
-            res.json({ info: 'Error during update user upvote count', error: error });
-          }
-
-          res.status(200).json({ info: 'Vote cleared successfully' });
-        })
-      })
+      clearUser(post.user_id, config, res);
     })
   })
 })
 // clear votes, comment
 router.delete('/comment', function(req, res) {
   Comment.findOne({ _id: req.body.comment_id }, function(error, comment) {
-    var type;
-
     if (error) {
       res.json({ info: 'Error during find comment' });
       return;
     }
+
     if (comment === null) {
       res.json({ info: 'Could not find comment' });
       return;
     }
-    var up = comment.upvotes.indexOf(req.currentUser.toString());
-    var down = comment.downvotes.indexOf(req.currentUser.toString());
-    if (up !== -1) {
-      comment.upvotes.splice(up, 1);
-      comment.score--;
-      type = 'upvotes';
-    }
 
-    if (down !== -1) {
-      comment.downvotes.splice(down, 1);
-      comment.score++;
-      type = 'downvotes';
+    var config = configure(comment, req.currentUser.toString());
+
+    if (config.type) {
+      comment = clearVotes(comment, req.currentUser.toString(), config);
     }
 
     comment.save(function(error) {
@@ -214,31 +221,7 @@ router.delete('/comment', function(req, res) {
         return;
       }
 
-
-      User.findOne({ _id: comment.user_id }, function(error, user) {
-
-        if (error) {
-          res.json({ info: 'Error during find user', error: error });
-        }
-
-        if (type === 'upvotes') {
-          user.upvotes--;
-          user.score--;
-        }
-
-        if (type === 'downvotes') {
-          user.downvotes--;
-          user.score++;
-        }
-
-        user.save(function(error) {
-          if (error) {
-            res.json({ info: 'Error during update user upvote count', error: error });
-          }
-
-          res.status(200).json({ info: 'Vote cleared successfully' });
-        })
-      })
+      clearUser(comment.user_id, config, res);
     })
   })
 })
