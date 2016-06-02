0d8756cc4538fee4dce4acec9603918c12fb60e0
Viet Le <viethle126@gmail.com>
2016-06-01 23:42:15 -0700

    Added Thread and Replies Directive #15

---
+++ public/directives/replies.directive.js
@@ -0,0 +1,21 @@
+var app = angular.module('post');
+
+app.directive('replies', replies);
+
+function replies($compile) {
+  return {
+    restrict: "E",
+    replace: true,
+    scope: {
+        reply: '='
+    },
+    templateUrl: 'directives/replies.directive.html',
+    link: function(scope, element, attributes) {
+      if (scope.reply.thread) {
+        $compile('<thread thread="reply.thread"></thread>')(scope, function(cloned, scope){
+           element.append(cloned);
+        });
+      }
+    }
+  }
+}
